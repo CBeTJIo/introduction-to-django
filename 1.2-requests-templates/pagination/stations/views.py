ea37962d199838801b7a0c8c9389a2b7577c4e68
Dmitriy_K <dimon_sdk@mail.ru>
2024-05-06 11:33:36 +0300

    Initial commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,31 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
-def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+file_name = # ссылка на файл data-398-2018-08-30.csv
 
+list_of_street = []
+with open(f'{file_name}', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        d = {'Name': row.get('Name'), 'Street': row.get('Street'), 'District': row.get('District')}
+        list_of_street.append(d)
+
+
+def bus_stations(request):
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(list_of_street, 10)
+    page = paginator.get_page(page_number)
+    station_list = paginator.page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': station_list,
+        'page': page
     }
+
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,30 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
-DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
-    },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
-    },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
-}
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def omlet(request):
+    qty = int(request.GET.get('servings', 1))
+
+    context = {
+        'recipe': {
+            'яйца, шт': f'{qty * 2}',
+            'молоко, л': f'{qty * 0.1}',
+            'соль, ч.л.': f'{qty * 0.5}',
+        },
+    }
+
+    return render(request, 'calculator/index.html', context)
+
+def pasta(request):
+    qty = int(request.GET.get('servings', 1))
+
+    context = {
+        'recipe': {
+            'макароны, г': f'{qty * 0.3}',
+            'сыр, г': f'{qty * 0.05}',
+        },
+    }
+
+    return render(request, 'calculator/index.html', context)
+
+def buter(request):
+    qty = int(request.GET.get('servings', 1))
+
+    context = {
+        'recipe': {
+            'хлеб, ломтик': f'{qty * 1}',
+            'колбаса, ломтик': f'{qty * 1}',
+            'сыр, ломтик': f'{qty * 1}',
+            'помидор, ломтик': f'{qty * 1}',
+        },
+    }
+
+    return render(request, 'calculator/index.html', context)
+
+
+
+def home_view(request):
+    pages = {
+        'Омлет': reverse('omlet'),
+        'Паста': reverse('pasta'),
+        'Бутерброд': reverse('buter')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, 'calculator/index.html', context)
