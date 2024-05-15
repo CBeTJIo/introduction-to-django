372c041e8e720bc87675f557a23f7f3a71adbd40
Dmitriy_K <dimon_sdk@mail.ru>
2024-05-15 23:23:59 +0300

    Update 1.2-requests-templates

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,55 +1,35 @@
 from django.shortcuts import render, reverse
 
 
-def omlet(request):
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+
+def food_view(request, food):
     qty = int(request.GET.get('servings', 1))
+    dict_product = {}
+    for dish in DATA:
+        if food == dish:
+            Ingredients = DATA[dish]
+            for product in Ingredients:
+                qty_product = Ingredients[product]
+                dict_product[product] = qty_product * qty
 
-    context = {
-        'recipe': {
-            'яйца, шт': f'{qty * 2}',
-            'молоко, л': f'{qty * 0.1}',
-            'соль, ч.л.': f'{qty * 0.5}',
-        },
-    }
+    context = {'recipe': dict_product}
 
-    return render(request, 'calculator/index.html', context)
-
-def pasta(request):
-    qty = int(request.GET.get('servings', 1))
-
-    context = {
-        'recipe': {
-            'макароны, г': f'{qty * 0.3}',
-            'сыр, г': f'{qty * 0.05}',
-        },
-    }
-
-    return render(request, 'calculator/index.html', context)
-
-def buter(request):
-    qty = int(request.GET.get('servings', 1))
-
-    context = {
-        'recipe': {
-            'хлеб, ломтик': f'{qty * 1}',
-            'колбаса, ломтик': f'{qty * 1}',
-            'сыр, ломтик': f'{qty * 1}',
-            'помидор, ломтик': f'{qty * 1}',
-        },
-    }
-
-    return render(request, 'calculator/index.html', context)
-
-
-
-def home_view(request):
-    pages = {
-        'Омлет': reverse('omlet'),
-        'Паста': reverse('pasta'),
-        'Бутерброд': reverse('buter')
-    }
-
-    context = {
-        'pages': pages
-    }
     return render(request, 'calculator/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from calculator.views import omlet, pasta, buter, home_view
+from calculator.views import food_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('omlet/', omlet, name='omlet'),
-    path('pasta/', pasta, name='pasta'),
-    path('buter/', buter, name='buter'),
+    path('<food>/', food_view, name='food')
 ]
