82f9fbb18ceb1cbe086034669b685e0256daf9e8
Dmitriy_K <dimon_sdk@mail.ru>
2024-06-10 11:51:30 +0300

    Change 3.2-crud

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -4,15 +4,12 @@ from logistic.models import Stock, Product, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
     class Meta:
         model = Product
         fields = '__all__'
 
 
-
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # positions = ProductSerializer(many=True)
     class Meta:
         model = StockProduct
         fields = ['product', 'quantity', 'price']
@@ -23,29 +20,18 @@ class StockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stock
-        fields = ['address ', 'positions']
-    # настройте сериализатор для склада
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
-        #return super().create(validated_data)
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
         for position in positions:
             StockProduct.objects.get_or_create(stock=stock, **position)
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
         for position in positions:
             StockProduct.objects.update_or_create(defaults={'quantity': position['quantity'], 'price': position['price']}, product=position['product'], stock=stock)
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
         return stock
